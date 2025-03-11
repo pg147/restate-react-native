211ef46bcf987d471fd362885ddc9443fe35290b
pg147 <xyzprathamesh147@gmail.com>
2025-03-12 00:15:12 +0530

    update: custom fonts loaded - mainLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "./globals.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
+    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    } 
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return <Stack />;
 }
